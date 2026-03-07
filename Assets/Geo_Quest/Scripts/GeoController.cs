2387a5935d7e8f690ff0f48415a8fbd094823bd6
pr1mr0se <leahisabellaperez@gmail.com>
2026-03-07 16:29:52 -0500

    colors

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,24 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GeoController : MonoBehaviour
 {
-    int var = 3;
-    string variableOne = "Hello";
+    private Rigidbody2D rb;
+    public int speed = 5;
+    public string nextLevel = "Scene Two";
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
-        string variableTwo = "World";
-        Debug.Log((variableOne + variableTwo));
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(var);
-        var++;
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            spriteRenderer.color = Color.red;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            spriteRenderer.color = Color.blue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            spriteRenderer.color = Color.green;
+        }
+
+        /*
         if (Input.GetKeyDown(KeyCode.W))
         {
             transform.position += new Vector3(0, 1, 0);
@@ -27,6 +46,31 @@ public class GeoController : MonoBehaviour
         {
             transform.position += new Vector3(0, -1, 0);
         }
-
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        */
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Death":
+                {
+                    string thisLevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thisLevel);
+                    break;
+                }
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextLevel);
+                    break;
+                }
+        }
     }
 }
